bfacf02386cfa3ea01f7f840e9f639a631fe1ce9
Jhon Fredy Santander <jsantander1219@gmail.com>
2024-09-14 11:51:11 -0500

    removing warn

--- lib/my_package_jh.dart
+++ lib/my_package_jh.dart
@@ -5,19 +5,19 @@ import 'package:flutter/material.dart';
 class MyCustomWidget extends StatelessWidget {
   final String text;
 
-  MyCustomWidget({required this.text});
+  const MyCustomWidget({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
         color: Colors.blue,
         borderRadius: BorderRadius.circular(8),
       ),
       child: Text(
         text,
-        style: TextStyle(color: Colors.white, fontSize: 18),
+        style: const TextStyle(color: Colors.white, fontSize: 18),
       ),
     );
   }
